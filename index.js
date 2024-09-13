4e1444bc1696934a6fed4007d17341feee8d766f
pyonpuni <157256898+pyonpuni@users.noreply.github.com>
2024-09-13 01:00:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('冬芽')
     .setDetails(`I・LOVE・YOU!!`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://i.postimg.cc/wTXgYgRr/Untitled1052_20240912203447.gif')
+    .setAssetsLargeImage('https://imgur.com/a/auNGy2W')
     .setAssetsLargeText('赤く色づくの♡')
     .addButton('大好き～!!', 'https://lit.link/fuyuesu');
 
