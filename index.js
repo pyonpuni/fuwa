ea95744f7322014eebfc4be694e55ecca9cab82d
pyonpuni <157256898+pyonpuni@users.noreply.github.com>
2024-09-15 12:12:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('冬芽')
     .setDetails(`I・LOVE・YOU!!`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('http://tiny.cc/discordapp')
+    .setAssetsLargeImage('https://i.postimg.cc/BQbKgBPB/Untitled1057-20240913193949.gif')
     .setAssetsLargeText('赤く色づくの♡')
     .addButton('大好き～!!', 'https://fuyume.bearblog.dev');
 
